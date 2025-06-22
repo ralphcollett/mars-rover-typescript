554fef71824bbfbee4950d9733f5d530615feea2
ralph.collett <ralph.collett@gmail.com>
2025-06-22 15:04:07 +0100

    First can move right.

--- test/mars-rover.test.ts
+++ test/mars-rover.test.ts
@@ -1,4 +1,9 @@
-const marsRover = (input: string): string => "1 2 N";
+const marsRover = (input: string): string => {
+  const inputLines = input.split("\n");
+  const robotActions = inputLines[2];
+  if (robotActions === "R") return "1 2 E"
+  return "1 2 N";
+};
 
 test('no actions means robot stays in starting position', () => {
   expect(marsRover(
@@ -7,3 +12,12 @@ test('no actions means robot stays in starting position', () => {
       ""
   )).toBe("1 2 N");
 });
+
+
+test('can move right', () => {
+  expect(marsRover(
+      "5 5\n" +
+      "1 2 N\n" +
+      "R"
+  )).toBe("1 2 E");
+})
